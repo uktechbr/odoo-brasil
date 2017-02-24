4dfa07cef935e8ab65e7e4e6d5bbea088b9bfbc2
martini97 <alessandrofmartini@gmail.com>
2017-02-24 18:18:27 -0300

    Implementação de importação de NFe, 1

--- br_nfe_import/wizard/import_nfe.py
+++ br_nfe_import/wizard/import_nfe.py
@@ -23,6 +23,109 @@ class WizardImportNfe(models.TransientModel):
     invoice_id = fields.Many2one('account.invoice', string='Fatura')
     serie = fields.Many2one('invoice.eletronic', string="Série")
 
+    def get_identificacao(self, nfe):
+        company = self.env['res.partner'].search([
+            ('cnpj_cpf', '=', nfe.NFe.infNFe.emit)], limit=1)
+        num_controle = int(''.join([str(SystemRandom().randrange(9))
+                           for i in range(8)]))
+        data_emissao = parser.parse(str(nfe.NFe.infNFe.ide.dhEmi))
+        data_fatura = parser.parse(str(nfe.NFe.infNFe.ide.dhSaiEnt))
+        ambiente = 'homologacao' if nfe.NFe.infNFe.ide.tpAmb == 2\
+            else 'producao'
+        finalidade_emissao = str(nfe.NFe.infNFe.ide.finNFe),
+        numero_inv = self.env['invoice.eletronic'].search_count([])
+        return dict(
+            code='AUTO' + (self.invoice_id.number or ''),
+            numero=numero_inv,
+            company_id=company.id,
+            name='Documento Eletrônico: nº ' + str(numero_inv),
+            serie=self.serie.id,
+            num_controle=num_controle,
+            data_emissao=data_emissao,
+            data_fatura=data_fatura,
+            ambiente=ambiente,
+            finalidade_emissao=finalidade_emissao
+        )
+
+    def get_total_values(self, nfe):
+        total = nfe.NFe.infNFe.total.ICMSTot
+        return dict(
+            valor_final=total.vNF,
+            valor_frete=total.vFrete,
+            valor_seguro=total.vSeg,
+            valor_bruto=total.vProd,
+            valor_desconto=total.vDesc,
+            valor_bc_icms=total.vBC,
+            valor_icms=total.vICMS,
+            valor_icms_deson=total.vICMSDeson,
+            valor_icmsst=total.vST,
+            valor_ii=total.vII,
+            valor_ipi=total.vIPI,
+            valor_pis=total.vPIS,
+            valor_cofins=total.vCOFINS,
+            valor_estimado_tributos=total.vTotTrib,
+        )
+
+    def get_main(self, nfe):
+        ide = nfe.NFe.infNFe.ide
+        dest = nfe.NFe.infNFe.dest
+        partner_doc = dest.CNPJ if hasattr(dest, 'CNPJ') else dest.CPF
+        partner = self.env['res.partner'].search([
+            ('cnpj_cpf', '=', partner_doc)])
+
+        return dict(
+            model=str(ide.mod),
+            invoice_id=self.invoice_id.id,
+            partner_id=partner.id,
+            tipo_operacao=str(ide.tpEmis),
+            payment_term_id=self.payment_term_id.id,
+            fiscal_position_id=self.fiscal_position_id,
+        )
+
+    def create_invoice_eletronic_item(self, item):
+        product = self.env['product.product'].search([
+            ('default_code', '=', item.prod.cProd)], limit=1)
+        if not product:
+            product = self.env['product.product'].search([
+                ('barcode', '=', item.prod.cEAN)], limit=1)
+        product_id = product.id
+        uom_id = self.env['product.uom'].search([
+            ('name', '=', item.prod.uCom)], limit=1).id
+        quantidade = item.prod.qCom
+        preco_unitario = item.prod.vUnCom
+        valor_bruto = item.prod.vProd
+        desconto = 0
+        if hasattr(item.prod, 'vDesc'):
+            desconto = item.prod.vDesc
+        seguro = 0
+        if hasattr(item.prod, 'vSeg'):
+            seguro = item.prod.vSeg
+        frete = 0
+        if hasattr(item.prod, 'vFrete'):
+            frete = item.prod.vFrete
+        outras_despesas = 0
+        if hasattr(item.prod, 'vOutro'):
+            outras_despesas = item.prod.vOutro
+        indicador_total = item.prod.indTot
+        tipo_produto = product.fiscal_type
+        cfop = item.prod.CFOP
+        ncm = item.prod.NCM
+        return self.env['invoice.eletronic.item'].create({
+            'product_id': product_id, 'uom_id': uom_id,
+            'quantidade': quantidade, 'preco_unitario': preco_unitario,
+            'valor_bruto': valor_bruto, 'desconto': desconto, 'seguro': seguro,
+            'frete': frete, 'outras_despesas': outras_despesas,
+            'indicador_total': indicador_total, 'tipo_produto': tipo_produto,
+            'cfop': cfop, 'ncm': ncm,
+        })
+
+    def get_items(self, nfe):
+        items = []
+        for det in nfe.NFe.infNFe.det:
+            item = self.create_invoice_eletronic_item(det)
+            items.append((4, item.id, False))
+        return {'eletronic_item_ids': items}
+
     @api.multi
     def action_import_nfe(self):
         if not self.nfe_xml:
@@ -30,53 +133,9 @@ class WizardImportNfe(models.TransientModel):
         nfe_string = base64.b64decode(self.nfe_xml)
         nfe = objectify.fromstring(nfe_string)
 
-        ide = nfe.NFe.infNFe.ide
-        emit = nfe.NFe.infNFe.emit
-        dest = nfe.NFe.infNFe.dest
-
-        if hasattr(dest, 'CNPJ'):
-            partner_doc = dest.CNPJ
-        else:
-            partner_doc = dest.CPF
-        partner = self.env['res.partner'].search([
-            ('cnpj_cpf', '=', partner_doc)])
-        company = self.env['res.company'].search([
-            ('cnpj_cpf', '=',  emit.CNPJ) ])
-
-        entrada_saida = 'saida' if ide.tpNF == 1 else 'entrada'
-
-        data_emissao = parser.parse(str(ide.dhEmi))
-        data_fatura = parser.parse(str(ide.dhSaiEnt))
-
-        numero_inv = self.env['invoice.eletronic'].search_count([])
-
-        ambiente = 'homologacao' if ide.tpAmb == 2 else 'producao'
-
-        num_controle = int(''.join([str(SystemRandom().randrange(9))
-                           for i in range(8)]))
-
-        invoice_eletronic = {
-            'state': 'draft',
-            'model': str(ide.mod),
-            'tipo_operacao': entrada_saida,
-            'invoice_id': self.invoice_id.id,
-            'payment_term_id': self.payment_term_id.id,
-            'partner_id': partner.id,
-            'fiscal_position_id': self.fiscal_position_id.id,
-            'code': 'AUTO ' + (self.invoice_id.number or ''),
-            'data_emissao': data_emissao,
-            'name': 'Documento Eletrônico: nº ' + str(numero_inv),
-            'data_fatura': data_fatura,
-            'company_id': company.id,
-            'serie': self.serie.id,
-            'ambiente': ambiente,
-            'numero': numero_inv,
-            'finalidade_emissao': str(ide.finNFe),
-            'tipo_emissao': str(ide.tpEmis),
-            'numero_controle': num_controle,
-            'ind_final': str(ide.indFinal),
-            'ind_pres': str(ide.indPres),
-            'ind_dest': str(ide.idDest),
-            'ind_ie_dest': str(dest.indIEDest),
-        }
-        self.env['invoice.eletronic'].create(invoice_eletronic)
+        invoice_dict = {}
+        invoice_dict.update(self.get_main(nfe))
+        invoice_dict.update(self.get_identificacao(nfe))
+        invoice_dict.update(self.get_total_values(nfe))
+        invoice_dict.update(self.get_items(nfe))
+        self.env['invoice.eletronic'].create(invoice_dict)
